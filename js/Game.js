467366ade88ffb5c7835639c848597bb7f75dc6e
MATHEUSORTEGA <94199495+MATHEUSORTEGA@users.noreply.github.com>
2022-08-06 18:01:23 -0300

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,380 @@
+class Game {
+  constructor() {
+    this.resetTitle = createElement("h2");
+    this.resetButton = createButton("");
+
+    this.leadeboardTitle = createElement("h2");
+
+    this.leader1 = createElement("h2");
+    this.leader2 = createElement("h2");
+    this.playerMoving = false;
+  }
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+    car1 = createSprite(width / 2 - 50, height - 100);
+    car1.addImage("car1", car1_img);
+    car1.scale = 0.07;
+
+    car2 = createSprite(width / 2 + 100, height - 100);
+    car2.addImage("car2", car2_img);
+    car2.scale = 0.07;
+
+    cars = [car1, car2];
+
+    fuels = new Group();
+    powerCoins = new Group();
+
+    obstacles = new Group();
+
+    var obstaclesPositions = [
+      { x: width / 2 + 250, y: height - 800, image: obstacle2Image },
+      { x: width / 2 - 150, y: height - 1300, image: obstacle1Image },
+      { x: width / 2 + 250, y: height - 1800, image: obstacle1Image },
+      { x: width / 2 - 180, y: height - 2300, image: obstacle2Image },
+      { x: width / 2, y: height - 2800, image: obstacle2Image },
+      { x: width / 2 - 180, y: height - 3300, image: obstacle1Image },
+      { x: width / 2 + 180, y: height - 3300, image: obstacle2Image },
+      { x: width / 2 + 250, y: height - 3800, image: obstacle2Image },
+      { x: width / 2 - 150, y: height - 4300, image: obstacle1Image },
+      { x: width / 2 + 250, y: height - 4800, image: obstacle2Image },
+      { x: width / 2, y: height - 5300, image: obstacle1Image },
+      { x: width / 2 - 180, y: height - 5500, image: obstacle2Image }
+    ];
+
+    // Adicionar sprite de combustível no jogo
+    this.addSprites(fuels, 4, fuelImage, 0.02);
+
+    // Adicionar sprite de moeda no jogo
+    this.addSprites(powerCoins, 18, powerCoinImage, 0.09);
+
+    //Adicionar sprite de obstáculo no jogo
+    this.addSprites(
+      obstacles,
+      obstaclesPositions.length,
+      obstacle1Image,
+      0.04,
+      obstaclesPositions
+    );
+  }
+
+  addSprites(spriteGroup, numberOfSprites, spriteImage, scale, positions = []) {
+    for (var i = 0; i < numberOfSprites; i++) {
+      var x, y;
+
+      //C41 //SA
+      if (positions.length > 0) {
+        x = positions[i].x;
+        y = positions[i].y;
+        spriteImage = positions[i].image;
+      } else {
+        x = random(width / 2 + 150, width / 2 - 150);
+        y = random(-height * 4.5, height - 400);
+      }
+      var sprite = createSprite(x, y);
+      sprite.addImage("sprite", spriteImage);
+
+      sprite.scale = scale;
+      spriteGroup.add(sprite);
+    }
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+
+    //C39
+    this.resetTitle.html("Reiniciar Jogo");
+    this.resetTitle.class("resetText");
+    this.resetTitle.position(width / 2 + 200, 40);
+
+    this.resetButton.class("resetButton");
+    this.resetButton.position(width / 2 + 230, 100);
+
+    this.leadeboardTitle.html("Placar");
+    this.leadeboardTitle.class("resetText");
+    this.leadeboardTitle.position(width / 3 - 60, 40);
+
+    this.leader1.class("leadersText");
+    this.leader1.position(width / 3 - 50, 80);
+
+    this.leader2.class("leadersText");
+    this.leader2.position(width / 3 - 50, 130);
+  }
+
+  play() {
+    this.handleElements();
+    this.handleResetButton();
+
+    Player.getPlayersInfo();
+    // chame a função aqui
+    player.getCarsAtEnd();
+
+    if (allPlayers !== undefined) {
+      image(track, 0, -height * 5, width, height * 6);
+
+      this.showFuelBar();
+      //chame a função aqui
+      this.showLife()
+      this.showLeaderboard();
+
+      //índice da matriz
+      var index = 0;
+      for (var plr in allPlayers) {
+        //adicione 1 ao índice para cada loop
+        index = index + 1;
+
+        //use os dados do banco de dados para exibir os carros nas direções x e y
+        var x = allPlayers[plr].positionX;
+        var y = height - allPlayers[plr].positionY;
+
+        cars[index - 1].position.x = x;
+        cars[index - 1].position.y = y;
+
+        if (index === player.index) {
+          stroke(10);
+          fill("red");
+          ellipse(x, y, 60, 60);
+
+          this.handleFuel(index);
+          this.handlePowerCoins(index);
+          this.handleObstacles(index);
+          this.CarsCollide(index);
+          //alterar a posição da câmera na direção y
+          camera.position.y = cars[index - 1].position.y;
+        }
+      }
+
+      if (this.playerMoving) {
+        player.positionY += 5;
+        player.update();
+      }
+
+      //manipulando eventos de teclado
+      this.handlePlayerControls();
+
+      //Linha de chegada
+      const finish = height*6-200
+      if(player.positionY>finish){
+        gameState = 2;
+        player.rank+=1
+        Player.updateCarsAtEnd(player.rank)
+        player.update();
+          this.showRank();
+        
+      }
+      
+
+      drawSprites();
+    }
+  }
+
+  handleResetButton() {
+    this.resetButton.mousePressed(() => {
+      database.ref("/").set({
+        playerCount: 0,
+        gameState: 0,
+        carsAtEnd: 0,
+        players: {},
+        // defina 0 para a variável
+      });
+      window.location.reload();
+    });
+  }
+
+  showLife() {
+    // crie a função aqui
+    push()
+    image(lifeImage,width/2+300,height-player.positionY+290,50,50)
+    fill("white")
+    rect(width/2+370,height-player.positionY+290,220,40)
+    fill("red")
+    rect(width/2+370,height-player.positionY+290,player.life,40)
+    pop()
+  }
+
+  showFuelBar() {
+    // crie a função aqui
+    push()
+    image(fuelImage,width/2-650,height-player.positionY+290,50,50)
+    fill("white")
+    rect(width/2-580,height-player.positionY+290,220,40)
+    fill("yellow")
+    rect(width/2-580,height-player.positionY+290,player.fuel,40)
+    pop()
+  }
+
+  showLeaderboard() {
+    var leader1, leader2;
+    var players = Object.values(allPlayers);
+    if (
+      (players[0].rank === 0 && players[1].rank === 0) ||
+      players[0].rank === 1
+    ) {
+      // &emsp;    Essa etiqueta é usada para exibir quatro espaços.
+      leader1 =
+        players[0].rank +
+        "&emsp;" +
+        players[0].name +
+        "&emsp;" +
+        players[0].score;
+
+      leader2 =
+        players[1].rank +
+        "&emsp;" +
+        players[1].name +
+        "&emsp;" +
+        players[1].score;
+    }
+
+    if (players[1].rank === 1) {
+      leader1 =
+        players[1].rank +
+        "&emsp;" +
+        players[1].name +
+        "&emsp;" +
+        players[1].score;
+
+      leader2 =
+        players[0].rank +
+        "&emsp;" +
+        players[0].name +
+        "&emsp;" +
+        players[0].score;
+    }
+
+    this.leader1.html(leader1);
+    this.leader2.html(leader2);
+  }
+
+  handlePlayerControls() {
+    if (keyIsDown(UP_ARROW)) {
+      this.playerMoving = true;
+      player.positionY += 10;
+      player.update();
+    }
+
+    if (keyIsDown(LEFT_ARROW) && player.positionX > width / 3 - 50) {
+      player.positionX -= 5;
+      player.update();
+    }
+
+    if (keyIsDown(RIGHT_ARROW) && player.positionX < width / 2 + 300) {
+      player.positionX += 5;
+      player.update();
+    }
+  }
+
+  handleFuel(index) {
+    //adicionando combustível
+    cars[index - 1].overlap(fuels, function(collector, collected) {
+      player.fuel = 185;
+      //o sprite é coletado no grupo de colecionáveis que desencadeou
+      //o evento
+      collected.remove();
+    });
+
+    // reduzindo o combustível do carro
+    if (player.fuel > 0 && this.playerMoving) {
+      player.fuel -= 0.9
+    }
+
+    if (player.fuel <= 0) {
+      gameState = 2;
+      this.gameOver();
+    }
+  }
+
+  handlePowerCoins(index) {
+    cars[index - 1].overlap(powerCoins, function(collector, collected) {
+      player.score += 21;
+      player.update();
+      //o sprite é coletado no grupo de colecionáveis que desencadeou
+      //o evento
+      collected.remove();
+    });
+    
+  }
+
+  handleObstacles(index){
+    cars[index - 1].overlap(obstacles, function(collector, collected) {
+      
+      collected.remove();
+      if(player.life>0){
+        player.life-=60
+      }
+        if (keyIsDown(LEFT_ARROW) && player.positionX > width / 3 - 50) {
+          player.positionX += 50;
+        }
+    
+        if (keyIsDown(RIGHT_ARROW) && player.positionX < width / 2 + 300) {
+          player.positionX -= 50;
+        }
+        player.update();
+    });
+  }
+
+  CarsCollide(index){
+    if(index==1){
+      if(cars[index - 1].collide(cars[1])){
+        if (keyIsDown(LEFT_ARROW) && player.positionX > width / 3 - 50) {
+          player.positionX += 50;
+        }  
+        if (keyIsDown(RIGHT_ARROW) && player.positionX < width / 2 + 300) {
+          player.positionX -= 50;
+        }  
+      }
+    }
+    if(index==2){
+      if(cars[index - 1].collide(cars[0])){
+        if (keyIsDown(LEFT_ARROW) && player.positionX > width / 3 - 50) {
+          player.positionX += 50;
+        }  
+        if (keyIsDown(RIGHT_ARROW) && player.positionX < width / 2 + 300) {
+          player.positionX -= 50;
+        }  
+      }
+    }
+    player.update();
+  }
+
+  showRank() {
+    swal({
+      title: `Incrível!${"\n"}${player.rank}º lugar`,
+      text: "Você alcançou a linha de chegada com sucesso!",
+      imageUrl:
+        "https://raw.githubusercontent.com/vishalgaddam873/p5-multiplayer-car-race-game/master/assets/cup.png",
+      imageSize: "100x100",
+      confirmButtonText: "Ok"
+    });
+  }
+
+  gameOver() {
+    swal({
+      title: `Fim de Jogo`,
+      text: "Oops você perdeu a corrida!",
+      imageUrl:
+        "https://cdn.shopify.com/s/files/1/1061/1924/products/Thumbs_Down_Sign_Emoji_Icon_ios10_grande.png",
+      imageSize: "100x100",
+      confirmButtonText: "Obrigado por jogar"
+    });
+  }
+}
